55ea8e1a65e9c7b03e7e4fe28af6bf13b3e54e32
Charlesboca <carlos1ariel@hotmail.com>
2026-03-26 13:23:43 -0300

    Cambio para que se abra en una pagina nueva

--- src/pages/WhatsAppButton.jsx
+++ src/pages/WhatsAppButton.jsx
@@ -2,9 +2,11 @@ function WhatsAppButton() {
   return (
     <a 
       href="https://api.whatsapp.com/send?phone=5493795337995&text=Hola!%20Quer%C3%ADa%20consultarles%20sobre"
-      className="whatsapp-flotante" /* Corregido: era class */
+      className="whatsapp-flotante"
+      target="_blank" 
+      rel="noopener noreferrer"
     >
-      <i className="fab fa-whatsapp"></i> {/* Corregido: era classname en minúscula */}
+      <i className="fab fa-whatsapp"></i>
     </a>
   );
 }
